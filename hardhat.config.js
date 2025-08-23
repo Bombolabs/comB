a07ffcd99228d2d59fceb96915163d293412bcd0
Hardcodepunk <howl@hardcodepunk.be>
2025-08-23 13:36:24 +0200

    configure bepolia testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
-
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.20",
+  networks: {
+    hardhat: {}, 
+    bepolia: {
+      url: "https://bepolia.rpc.berachain.com",
+      chainId: 80069,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {},
 };
